640d3d1728ee624861e2b71f7a551c7e91020447
Matt Marcum <matthew.marcum@activenetwork.com>
2016-07-16 16:22:09 -0500

    yuidocs first pass

--- addon/services/superlogin.js
+++ addon/services/superlogin.js
@@ -4,6 +4,12 @@ import config from 'ember-get-config';
 
 const { server }= config['ember-cli-superlogin'];
 
+/**
+* Superlogin provides a service abstracting all of the restful api endpoints
+* for the [node Superlogin server](https://www.npmjs.com/package/superlogin).
+*
+* @class Superlogin
+*/
 export default Ember.Service.extend({
   testMirage() {
     fetch(`${server}/session`).then(response => console.log);
