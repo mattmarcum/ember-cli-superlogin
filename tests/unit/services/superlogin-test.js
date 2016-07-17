8464e1dede34deef2920483579a0ad63ea1e3628
Matt Marcum <matt@strangelooper.com>
2016-07-17 02:54:34 -0500

    finish validate-username

--- tests/unit/services/superlogin-test.js
+++ tests/unit/services/superlogin-test.js
@@ -5,6 +5,8 @@ import {
   it
 } from 'ember-mocha';
 
+import mirageStarter from '../../helpers/ember-mirage-starter';
+
 describeModule(
   'service:superlogin',
   'SuperloginService',
@@ -13,19 +15,22 @@ describeModule(
     // needs: ['service:foo']
   },
   function() {
+    before(function() {
+      mirageStarter(this.container);
+    });
     // Replace this with your real tests.
     it('returns a rejected promise on invalid username', function() {
       let service = this.subject();
 
-      const promise = service.validateUsername('test');
-
+      const promise = service.validateUsername('asdf');
+      return expect(promise).to.be.rejected;
     });
 
     it('returns a resolved promise on valid username', function() {
       let service = this.subject();
 
       const promise = service.validateUsername('superlogin');
-
+      return expect(promise).to.be.fulfilled;
     });
   }
 );
